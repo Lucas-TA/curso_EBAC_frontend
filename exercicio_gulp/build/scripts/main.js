a9ffdbefb7eed749af9fd17ccb5cc7dc72a20526
Lucas-TA <dev.ascencao@gmail.com>
2023-11-30 19:25:47 -0300

    finished task

---
+++ exercicio_gulp/build/scripts/main.js
@@ -0,0 +1,1 @@
+const dropContainer=document.getElementById("dropdown-item"),button=document.querySelector("button");function showComment(){dropContainer.classList.toggle("active")}button.addEventListener("click",showComment);
