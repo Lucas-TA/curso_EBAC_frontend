39b922b909c674135403ef239a3b1500e2747a6a
Lucas-TA <dev.ascencao@gmail.com>
2023-12-18 19:51:23 -0300

    finished first email class

--- Aulas/Grunt/gruntfile.js
+++ Aulas/Grunt/gruntfile.js
@@ -63,6 +63,7 @@ module.exports = function(grunt) {
                             replacement: './scripts/main.min.js'
                         },
                     ]
+                    
                 },
                 files: [
                     {
