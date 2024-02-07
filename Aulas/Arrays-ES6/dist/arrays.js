fb7c89e1e489db51c3ed8d0582b0950c40987fa9
Lucas-TA <dev.ascencao@gmail.com>
2024-02-06 21:45:43 -0300

    initial commit

---
+++ Aulas/Arrays-ES6/dist/arrays.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const redesSociais = ['Facebook', 'Instagram', 'Youtube'];
+for (let i = 0; i < redesSociais.length; i++) {
+  console.log("Eu tenho uma conta na rede social ".concat(redesSociais[i]));
+}
---
+++ Aulas/Arrays-ES6/src/arrays.js
@@ -0,0 +1,9 @@
+const redesSociais = ['Facebook', 'Instagram', 'Youtube'];
+
+// for (let i = 0; i < redesSociais.length; i++) {
+//     console.log(`Eu tenho uma conta na rede social ${redesSociais[i]}`);
+// }
+
+redesSociais.forEach(item => {
+    console.log(`Eu tenho uma conta na rede social ${item}`);
+})
